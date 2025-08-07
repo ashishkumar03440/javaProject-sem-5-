bda1750cdc1202a3541607df649f854f225021c2
ashish03440 <ashishkumar03440@gmail.com>
2025-08-07 14:46:24 +0530

    add function added

---
+++ calculator.java
@@ -0,0 +1,11 @@
+public class calculator {
+
+    public static int add(int a , int b ){
+        return a+b;
+    }
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 10;
+        System.out.println(add(a, b));
+    }
+}
