5c3cc1f1240b69706a319f9fed1700387ad6140a
Nimit Goyal <nimitgoyal260405@gmail.com>
2025-08-07 14:57:25 +0530

    Remove all files and keep only calculator2.js

---
+++ Desktop/js proj/calculator2.java
@@ -0,0 +1,12 @@
+public class calculator2 {
+
+    public static int mul(int a , int b ){
+        return a*b;
+    }
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 10;
+        System.out.println(mul(a, b));
+    }
+
+}
